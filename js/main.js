109fa1eff449b021f0eee91b352d273fe03ae48f
BukanAhmadFauzi <achmadchocs96@gmail.com>
2021-03-21 10:27:02 +0700

    penambahan new dan gallery

--- js/main.js
+++ js/main.js
@@ -1,4 +1,12 @@
 // navbar
+$('#menu').onePageNav({
+    currentClass: 'active',
+    changeHash: false,
+    scrollSpeed: 750,
+    scrollThreshold: 0.5,
+    filter: '',
+    easing: 'swing',
+});
 $(document).scroll(function(){
     if ($(window).scrollTop() > 50) {
         $('#navbar').removeClass('bg-nontrans').addClass('bg-dark');
